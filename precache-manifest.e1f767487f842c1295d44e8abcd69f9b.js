8030cdd2e8a2840f4a82085fa302afd2db66b756
abusalehkhan12 <abusaleh.khan12@gmail.com>
2020-05-10 21:31:28 +0530

    Updates

---
+++ precache-manifest.e1f767487f842c1295d44e8abcd69f9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a951650c0db164cf5be609e012e8a46",
+    "url": "/Provido-intern/index.html"
+  },
+  {
+    "revision": "7073298115db05e4804d",
+    "url": "/Provido-intern/static/css/main.94bccf75.chunk.css"
+  },
+  {
+    "revision": "d21fd558b0683bcd2b97",
+    "url": "/Provido-intern/static/js/2.b3721bce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Provido-intern/static/js/2.b3721bce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7073298115db05e4804d",
+    "url": "/Provido-intern/static/js/main.b4a911e5.chunk.js"
+  },
+  {
+    "revision": "7496483c1e8c703262ca",
+    "url": "/Provido-intern/static/js/runtime-main.c8bbf363.js"
+  }
+]);
